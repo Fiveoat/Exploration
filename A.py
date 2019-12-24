f8c47412e6257fe0201c8ae2c315a010c33308cb
Coty Fivecoat <Fiveoat@gmail.com>
2019-12-23 22:46:46 -0700

    Update A.py

--- A.py
+++ A.py
@@ -1,1 +1,19 @@
-#this can be a test file
+cat = {"name" : "cat", "weight" : "fat", "age" : 10}
+
+print(cat)
+# cat.clear()
+#clears dict contents
+
+cat_2 = cat.copy()
+print(cat_2)
+#copies dict
+
+print(cat == cat_2)
+
+cats = {}.fromkeys(["cat", "age", "fat", "tacos"], "unknown")
+print(cats)
+# makes a dict with default values
+
+name = cats.get("name")
+print(name)
+
